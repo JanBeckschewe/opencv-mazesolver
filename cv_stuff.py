a2f8ed2ebeacce245d84bdcf76b3768a95831143
Jan Beckschewe <jan.becksy@gmail.com>
2018-02-25 16:45:46 +0100

    Add PD control and move the cv stuff to a separate file

---
+++ cv_stuff.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import yaml
+
+with open("camera_calibration_values.yaml") as calibration_matrix:
+    cal_mat = yaml.load(calibration_matrix)
+
+np_camera_matrix = np.array(cal_mat['camera_matrix'])
+np_distortion_coefficients = np.array(cal_mat['distortion_coefficients'])
+
+
+def modify_image(frame):
+    img = frame.array
+    img = cv2.rotate(img, 1)
+    img = cv2.undistort(img, np_camera_matrix, np_distortion_coefficients)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img_blurred = cv2.GaussianBlur(img_gray, (15, 15), 0)
+    img_sharpened = cv2.addWeighted(img_gray, 1.5, img_blurred, -0.5, 0)
+
+    img_canny = cv2.Canny(img_sharpened, 50, 80)
+
+    lines = cv2.HoughLinesP(image=img_canny, rho=1, theta=np.pi / 180, threshold=60, minLineLength=50,
+                            maxLineGap=10)
+
+    img_canny = cv2.cvtColor(img_canny, cv2.COLOR_GRAY2BGR)
+    return img_canny, lines
--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import time
 
 import cv2
-import numpy as np
-import yaml
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
+import cv_stuff
 import linecalc
 import motors
 
-with open("camera_calibration_values.yaml") as calibration_matrix:
-    cal_mat = yaml.load(calibration_matrix)
+proportional_const, derivate_const = 0.2, 1
+last_error = 0
 
-np_camera_matrix = np.array(cal_mat['camera_matrix'])
-np_distortion_coefficients = np.array(cal_mat['distortion_coefficients'])
-
-w, h = 600, 450
+w, h = 640, 480
 
 camera = PiCamera()
 camera.resolution = (w, h)
@@ -26,21 +22,8 @@ rawCapture = PiRGBArray(camera, size=(w, h))
 time.sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    img = frame.array
-    img = cv2.rotate(img, 1)
-    img = cv2.undistort(img, np_camera_matrix, np_distortion_coefficients)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # img_masked = cv2.inRange(img, 0, 50)
 
-    img_blurred = cv2.GaussianBlur(img_gray, (15, 15), 0)
-    img_sharpened = cv2.addWeighted(img_gray, 1.5, img_blurred, -0.5, 0)
-
-    img_canny = cv2.Canny(img_sharpened, 30, 60)
-
-    lines = cv2.HoughLinesP(image=img_canny, rho=1, theta=np.pi / 180, threshold=60, minLineLength=50,
-                            maxLineGap=10)
-
-    img_canny = cv2.cvtColor(img_canny, cv2.COLOR_GRAY2BGR)
+    img_canny, lines = cv_stuff.modify_image(frame)
 
     averageLinePosition = w / 2
     i = 0
@@ -53,25 +36,27 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                 if linecalc.is_line_horizontal(x1, x2, y1, y2):
                     is_horizontal_line_near = True
 
-                    # does not actually work because the turn will start too early and only find the new line back after
-                    # about 20 to 30 cm, which is way too much for our small maze
-                    # can maybe get solved with a delay
-                    if (x1 < w * .4 or x2 < w * .4) and (y1 > h * .85 or y2 > h * .85):
+                    if (x1 < w * .4 or x2 < w * .4) and (y1 > h * .5 or y2 > h * .5):
                         is_horizontal_line_left = True
 
+                    # draw a blue line for horizontal lines
                     cv2.line(img_canny, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
                 else:
                     averageLinePosition = averageLinePosition + (((x1 + x2) / 2) - averageLinePosition) / (i + 1)
+                    # draw a green line for vertical lines
                     cv2.line(img_canny, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
         if is_horizontal_line_near and is_horizontal_line_left:
-            motors_steer = -1
+            motor_steer = -1
         else:
-            motors_steer = (w / 2 + (averageLinePosition - w)) / w * 2
+            # PD control
+            pos_to_mid = (w / 2 + (averageLinePosition - w)) / w * 2
+            motor_steer = proportional_const * pos_to_mid + derivate_const * (pos_to_mid - last_error)
+            last_error = pos_to_mid
 
-        print(motors_steer)
-        motors.set_speed_from_speed_steer(1, motors_steer)
+        print(motor_steer)
+        motors.set_speed_from_speed_steer(.35, motor_steer)
     else:
         motors.set_speed_from_speed_steer(0, 0)
 
