7102233bf6be5deeb8c496486d30282ffb8711d6
Jan Beckschewe <jan.becksy@gmail.com>
2018-02-25 12:47:33 +0100

    Place the camera on the bottom of the vehicle and therefore use the calibration values to undistort the image

--- main.py
+++ main.py
@@ -2,42 +2,18 @@ import time
 
 import cv2
 import numpy as np
+import yaml
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
 import linecalc
 import motors
 
-path = []
-left, right, forward, backward = range(4)
+with open("camera_calibration_values.yaml") as calibration_matrix:
+    cal_mat = yaml.load(calibration_matrix)
 
-
-def simplify_maze(turn):
-    if len(path) < 3 or path[len(path) - 2] != backward:
-        return
-
-    total_angle = 0
-
-    for x in range(1, 3):
-        if path[len(path) - x] == right:
-            total_angle += 90
-        elif path[len(path) - x] == left:
-            total_angle += 270
-        elif path[len(path) - x] == backward:
-            total_angle += 180
-
-    total_angle = total_angle % 360
-    path.pop(2)
-
-    if total_angle == 0:
-        path[len(path) - 1] = forward
-    elif total_angle == 90:
-        path[len(path) - 1] = right
-    elif total_angle == 180:
-        path[len(path) - 1] = backward
-    elif total_angle == 270:
-        path[len(path) - 1] = left
-
+np_camera_matrix = np.array(cal_mat['camera_matrix'])
+np_distortion_coefficients = np.array(cal_mat['distortion_coefficients'])
 
 w, h = 600, 450
 
@@ -52,28 +28,19 @@ time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     img = frame.array
     img = cv2.rotate(img, 1)
+    img = cv2.undistort(img, np_camera_matrix, np_distortion_coefficients)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img_masked = cv2.inRange(img, 0, 50)
 
-    src = np.array([[.36 * w, .65 * h], [.64 * w, .65 * h], [w - 1, h - 1], [0, h - 1]], dtype=np.float32)
-    dst = np.array([[0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1]], dtype=np.float32)
+    img_blurred = cv2.GaussianBlur(img_gray, (15, 15), 0)
+    img_sharpened = cv2.addWeighted(img_gray, 1.5, img_blurred, -0.5, 0)
 
-    img_warped = cv2.warpPerspective(img_gray, cv2.getPerspectiveTransform(src, dst), (w, h))
-    img_warped_masked = cv2.inRange(img_warped, 0, 65)
+    img_canny = cv2.Canny(img_sharpened, 30, 60)
 
-    img_warped_blurred = cv2.GaussianBlur(img_warped_masked, (15, 15), 0)
-    img_warped_sharpened = cv2.addWeighted(img_warped_masked, 1.5, img_warped_blurred, -0.5, 0)
-
-    img_warped_canny = cv2.Canny(img_warped_sharpened, 30, 60)
-
-    lines = cv2.HoughLinesP(image=img_warped_canny, rho=1, theta=np.pi / 180, threshold=60, minLineLength=50,
+    lines = cv2.HoughLinesP(image=img_canny, rho=1, theta=np.pi / 180, threshold=60, minLineLength=50,
                             maxLineGap=10)
 
-    img_warped_canny = cv2.cvtColor(img_warped_canny, cv2.COLOR_GRAY2BGR)
-
-    # Finds the line in the most simplistic possible way: Look for the side of the image where more lines start and
-    # end on and move in that direction. ̶i̶̶t̶̶ ̶̶f̶̶a̶̶i̶̶l̶̶s̶̶ ̶̶w̶̶h̶̶e̶̶n̶̶ ̶̶t̶̶h̶̶e̶̶r̶̶e̶̶ ̶̶i̶̶s̶̶ ̶̶a̶̶
-    # ̶̶c̶̶r̶̶o̶̶s̶̶s̶̶i̶̶n̶̶g̶̶ ̶̶o̶̶f̶̶ ̶̶l̶̶i̶̶n̶̶e̶̶s̶ but on a single line with not so strong curves it works
-    # ̶i̶̶t̶̶ ̶̶c̶̶a̶̶n̶̶ ̶̶g̶̶e̶̶t̶̶ ̶̶o̶̶p̶̶t̶̶i̶̶m̶̶i̶̶z̶̶e̶̶d̶̶ ̶̶w̶̶i̶̶t̶̶h̶̶ ̶̶p̶̶d̶̶ ̶̶c̶̶o̶̶n̶̶t̶̶r̶̶o̶̶l̶̶.̶
+    img_canny = cv2.cvtColor(img_canny, cv2.COLOR_GRAY2BGR)
 
     averageLinePosition = w / 2
     i = 0
@@ -92,11 +59,11 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                     if (x1 < w * .4 or x2 < w * .4) and (y1 > h * .85 or y2 > h * .85):
                         is_horizontal_line_left = True
 
-                    cv2.line(img_warped_canny, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                    cv2.line(img_canny, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
                 else:
                     averageLinePosition = averageLinePosition + (((x1 + x2) / 2) - averageLinePosition) / (i + 1)
-                    cv2.line(img_warped_canny, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.line(img_canny, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
         if is_horizontal_line_near and is_horizontal_line_left:
             motors_steer = -1
@@ -108,7 +75,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     else:
         motors.set_speed_from_speed_steer(0, 0)
 
-    cv2.imshow("Frame", img_warped_canny)
+    cv2.imshow("Frame", img_canny)
     key = cv2.waitKey(1) & 0xFF
 
     rawCapture.truncate(0)
---
+++ maze_stuff.py
@@ -0,0 +1,29 @@
+path = []
+left, right, forward, backward = range(4)
+
+
+def simplify_maze(turn):
+    if len(path) < 3 or path[len(path) - 2] != backward:
+        return
+
+    total_angle = 0
+
+    for x in range(1, 3):
+        if path[len(path) - x] == right:
+            total_angle += 90
+        elif path[len(path) - x] == left:
+            total_angle += 270
+        elif path[len(path) - x] == backward:
+            total_angle += 180
+
+    total_angle = total_angle % 360
+    path.pop(2)
+
+    if total_angle == 0:
+        path[len(path) - 1] = forward
+    elif total_angle == 90:
+        path[len(path) - 1] = right
+    elif total_angle == 180:
+        path[len(path) - 1] = backward
+    elif total_angle == 270:
+        path[len(path) - 1] = left
