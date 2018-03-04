82c180c239b8f4b95e73f3b68cd63f938d0fcb54
Jan Beckschewe <jan.becksy@gmail.com>
2018-03-04 21:35:44 +0100

    Add installing script

---
+++ INSTALL.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
+conda install -c conda-forge opencv
+pip install pyyaml
+pip install git+https://github.com/dpallot/simple-websocket-server.git
+
+echo 'all done.'
