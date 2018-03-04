8ef607c4fa08efc0ba7c1c97f84bd56c0c006405
Jan Reimer <flush625@gmail.com>
2018-03-04 22:27:33 +0100

    Implemented basic directions

--- RaspberryMazeSolver/linecalc.py
+++ RaspberryMazeSolver/linecalc.py
@@ -2,5 +2,19 @@ import math
 
 
 def is_line_horizontal(x1, x2, y1, y2):
+    # https://www.mathebibel.de/steigungswinkel
     angle = math.atan2(y2 - y1, x2 - x1) * 180.0 / math.pi
     return -45 < angle < 45
+
+
+def is_line_vertical(x1, x2, y1, y2):
+    angle = math.atan2(y2 - y1, x2 - x1) * 180.0 / math.pi
+    return 60 < angle < 120
+
+
+def is_line_left(x1, x2, y1, y2, h, w):
+    return (x1 < w * .4 or x2 < w * .4) and (y1 > h * .5 or y2 > h * .5)
+
+
+def is_line_right(x1, x2, y1, y2, h, w):
+    return (x1 > w * .4 or x2 > w * .4) and (y1 > h * .5 or y2 > h * .5)
--- RaspberryMazeSolver/main.py
+++ RaspberryMazeSolver/main.py
@@ -4,9 +4,17 @@ import cv2
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
-import cv_stuff
-import linecalc
-import motors
+from RaspberryMazeSolver import cv_stuff
+from RaspberryMazeSolver import linecalc
+from RaspberryMazeSolver import motors
+
+is_first_run = False
+left, right, forward, backward = range(4)
+direction = forward
+simple_path = []
+simple_path_position = 0
+path = []
+path_position = 0
 
 proportional_const, derivate_const = 0.2, 1
 last_error = 0
@@ -27,43 +35,60 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     averageLinePosition = w / 2
     i = 0
-    is_horizontal_line_near = False
+    # noinspection PyRedeclaration
+    is_horizontal_line_right = False
     is_horizontal_line_left = False
+    # noinspection PyRedeclaration
+    is_vertical_line = False
 
-    if lines is not None:
+    if lines is None:
+        path.append(backward)
+    else:
         for line in lines:
             for x1, y1, x2, y2 in line:
-                if linecalc.is_line_horizontal(x1, x2, y1, y2):
-                    is_horizontal_line_near = True
+                averageLinePosition = averageLinePosition + (((x1 + x2) / 2) - averageLinePosition) / (i + 1)
+                if linecalc.is_line_horizontal(x1, x2, y1, y2) and linecalc.is_line_left(x1, x2, y1, y2, h, w):
+                    is_horizontal_line_left = True
 
-                    if (x1 < w * .4 or x2 < w * .4) and (y1 > h * .5 or y2 > h * .5):
-                        is_horizontal_line_left = True
-
-                    # draw a blue line for horizontal lines
+                    # draw a blue line for left horizontal lines
                     cv2.line(img_canny, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-                else:
-                    averageLinePosition = averageLinePosition + (((x1 + x2) / 2) - averageLinePosition) / (i + 1)
+                if linecalc.is_line_horizontal(x1, x2, y1, y2) and linecalc.is_line_right(x1, x2, y1, y2, h, w):
+                    is_horizontal_line_right = True
+
+                    # draw a red line for right horizontal lines
+                    cv2.line(img_canny, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+                if linecalc.is_line_vertical(x1, x2, y1, y2):
+                    is_vertical_line = True
+
                     # draw a green line for vertical lines
                     cv2.line(img_canny, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        if is_horizontal_line_near and is_horizontal_line_left:
-            motor_steer = -1
+        if is_horizontal_line_left:
+            path.append(left)
+
+        elif is_vertical_line and is_horizontal_line_right:
+            path.append(forward)
+
+        elif is_horizontal_line_right:
+            path.append(right)
+
         else:
-            # PD control
             pos_to_mid = (w / 2 + (averageLinePosition - w)) / w * 2
             motor_steer = proportional_const * pos_to_mid + derivate_const * (pos_to_mid - last_error)
             last_error = pos_to_mid
 
         print(motor_steer)
         motors.set_speed_from_speed_steer(.35, motor_steer)
-    else:
-        motors.set_speed_from_speed_steer(0, 0)
 
-    cv2.imshow("Frame", img_canny)
-    key = cv2.waitKey(1) & 0xFF
+        cv2.imshow("Frame", img_canny)
+        key = cv2.waitKey(1) & 0xFF
 
-    rawCapture.truncate(0)
+        rawCapture.truncate(0)
 
-    if key == ord("q"):
-        break
+        if key == ord("q"):
+            break
+
+
+
