cbda6f591f8f374bd0f7dfa63ebc33c96e817bcd
Jan Beckschewe <jan.becksy@gmail.com>
2018-03-29 23:19:15 +0200

    Rebuild the main direction choosing code

--- src/main.py
+++ src/main.py
@@ -30,8 +30,7 @@ saw_right_turn_last_frame = False
 
 current_direction = maze.forward
 
-for frame in camera.capture_continuous(rawCapture, format="bgr",
-                                       use_video_port=True):
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
     img_canny, lines, num_black_pixels = opencv.modify_image(frame)
 
@@ -45,92 +44,85 @@ for frame in camera.capture_continuous(rawCapture, format="bgr",
     if lines is not None:
         for line in lines:
             for x1, y1, x2, y2 in line:
-                averageLinePosition += ((x1 + x2) / 2
-                                        - averageLinePosition) / (i + 1)
+                averageLinePosition += ((x1 + x2) / 2 - averageLinePosition) / (i + 1)
                 i += 1
 
                 opencv.find_lines(img_canny, x1, y1, x2, y2, w, h,
                                   are_turns_seen_rn)
 
-    if not maze.is_paused:
+    # when reset from website
+    if is_finished and len(maze.full_path) == 0:
+        is_finished = False
+
+    # when reached end
+    if not is_finished and num_black_pixels > w * h * .5:
+        is_finished = True
+        print("is finished")
+
+    if not maze.is_paused and not is_finished:
         if not is_first_run:
-            oppsite_direction = maze.simple_path[maze.simple_path_position]
-            current_direction = maze.forward \
-                if oppsite_direction == maze.forward \
-                else abs(2 - oppsite_direction)
+            opposite_direction = maze.simple_path[maze.simple_path_position]
+            current_direction = maze.forward if opposite_direction == maze.forward else abs(2 - opposite_direction)
 
-        # when reset from website
-        if is_finished and len(maze.full_path) == 0:
-            is_finished = False
-
-        # when reached end
-        if not is_finished and num_black_pixels > w * h * .5:
-            is_finished = True
-            print("is finished")
-
-        if is_finished:
-            pass
-        elif lines is None:
-            if not current_direction == maze.backward:
+        if lines is None:
+            if current_direction != maze.backward:
                 maze.add_turn(maze.backward)
-            motor_steer = 2
-            current_direction = maze.backward
+                current_direction = maze.backward
 
-        else:
-            if current_direction == maze.backward:
-                motor_steer = 2
-                if are_turns_seen_rn[maze.forward]:
-                    current_direction = maze.forward
-                elif are_turns_seen_rn[maze.left] or is_in_backwards_left_turn:
-                    is_in_backwards_left_turn = True
-                    motor_steer = -2
-            else:
-                is_in_backwards_left_turn = False
-
-            if ((current_direction == maze.right
-                 and not are_turns_seen_rn[maze.right])
-                or (current_direction == maze.right
-                    and not are_turns_seen_rn[maze.left])) \
-                    and are_turns_seen_rn[maze.forward]:
+        if current_direction == maze.backward:
+            if are_turns_seen_rn[maze.forward]:
                 current_direction = maze.forward
-
             if are_turns_seen_rn[maze.left]:
-                if not current_direction == maze.left:
-                    if is_first_run:
-                        maze.add_turn(maze.left)
-                    else:
-                        maze.simple_path_position -= 1
-                    current_direction = maze.left
-                motor_steer = -2
-            elif are_turns_seen_rn[maze.forward]:
-                if are_turns_seen_rn[maze.right]:
-                    if not saw_right_turn_last_frame:
-                        if is_first_run:
-                            maze.add_turn(maze.forward)
-                            current_direction = maze.forward
-                        else:
-                            maze.simple_path_position -= 1
+                is_in_backwards_left_turn = True
+        else:
+            is_in_backwards_left_turn = False
 
-                pos_to_mid = (w / 2 + (averageLinePosition - w)) / w * 2
+        if is_in_backwards_left_turn:
+            motor_steer = -2
 
-                motor_steer = pid_control.pid_drive(pos_to_mid)
-            elif are_turns_seen_rn[maze.right]:
-                if not current_direction == maze.right:
-                    if is_first_run:
-                        maze.add_turn(maze.right)
-                        current_direction = maze.right
-                    else:
-                        maze.simple_path_position -= 1
-                motor_steer = 2
+        if ((current_direction == maze.left and not are_turns_seen_rn[maze.left]) or (
+                current_direction == maze.right and not are_turns_seen_rn[maze.right])) and are_turns_seen_rn[maze.forward]:
+            current_direction = maze.forward
+
+        if current_direction == maze.forward:
+            if are_turns_seen_rn[maze.left] and current_direction != maze.left:
+                new_turn = maze.left
+            elif are_turns_seen_rn[maze.forward] and are_turns_seen_rn[maze.right] and not saw_right_turn_last_frame:
+                saw_right_turn_last_frame = True
+                new_turn = maze.forward
+            elif are_turns_seen_rn[maze.right] and current_direction != maze.right:
+                new_turn = maze.right
             else:
-                print("something went wrong")
+                new_turn = None
 
-            if not are_turns_seen_rn[maze.right]:
-                saw_right_turn_last_frame = False
+            if new_turn is not None:
+                if is_first_run:
+                    maze.add_turn(new_turn)
+                    current_direction = new_turn
+                else:
+                    maze.simple_path_position -= 1
 
-        motors.set_speed_from_speed_steer(
-            0 if is_finished else .4, motor_steer)
+        if not are_turns_seen_rn[maze.right]:
+            saw_right_turn_last_frame = False
 
+        # main driving part
+        if current_direction == maze.left:
+            motor_steer = -2
+        elif current_direction == maze.forward:
+            pos_to_mid = (w / 2 + (averageLinePosition - w)) / w * 2
+
+            motor_steer = pid_control.pid_drive(pos_to_mid)
+        elif current_direction == maze.right:
+            motor_steer = 2
+        elif current_direction == maze.backward:
+            if is_in_backwards_left_turn:
+                motor_steer = -2
+            else:
+                motor_steer = 2
+        else:
+            print("something went horrible")
+
+        motors.set_speed_from_speed_steer(.28, motor_steer)
     else:
         motors.set_speed(0, 0)
 
